b38e6e70349c420806ebb321618b5925a1b8fe65
LopesACF <altemilsonlopes@gmail.com>
2023-03-31 20:16:50 -0300

    .

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Qual o seu nome? ");
+String nome = Console.ReadLine()!;
+Console.Write("Qual seu sobrenome? ");
+String sobrenome = Console.ReadLine()!;
+
+String NomeSobrenome = $"{nome} {sobrenome}";
+
+Console.WriteLine($"Olá, Boa noite {NomeSobrenome}");
